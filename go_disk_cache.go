f998bf8324c20f1f4761f76a557a807906cf1891
Robert Mulley <rmulley@gmail.com>
2013-12-13 12:43:52 -0500

    Working on ability to pick custom directory for cache files

--- go_disk_cache.go
+++ go_disk_cache.go
@@ -10,7 +10,8 @@ import (
 ) //import
 
 type GoDiskCache struct {
-	Keys map[string]cacheFile
+	keys      map[string]cacheFile
+	directory string
 } //struct
 
 type cacheFile struct {
@@ -18,8 +19,18 @@ type cacheFile struct {
 	lifeTime int
 } //struct
 
-func New() *GoDiskCache {
-	return &GoDiskCache{Keys: make(map[string]cacheFile)}
+type Params struct {
+	param map[string]interface{}
+} //struct
+
+func New(p Params) *GoDiskCache {
+	var directory string = os.TempDir()
+
+	if _, ok := p.param["directory"]; ok {
+		directory = p.param["directory"].(string)
+	} //if
+
+	return &GoDiskCache{keys: make(map[string]cacheFile), directory: directory}
 } //New
 
 func (dc *GoDiskCache) Get(key string) (string, error) {
@@ -32,11 +43,11 @@ func (dc *GoDiskCache) Get(key string) (string, error) {
 	}() //func
 
 	//open the cache file
-	if file, err := os.Open(os.TempDir() + dc.Keys[key].fileName); err == nil {
+	if file, err := os.Open(dc.directory + dc.keys[key].fileName); err == nil {
 		//get stats about the file, need modified time
 		if fi, err := file.Stat(); err == nil {
 			//check that cache file is still valid
-			if int(time.Since(fi.ModTime()).Seconds()) < dc.Keys[key].lifeTime {
+			if int(time.Since(fi.ModTime()).Seconds()) < dc.keys[key].lifeTime {
 				//try reading entire file
 				if data, err := ioutil.ReadAll(file); err == nil {
 					return string(data), err
@@ -66,13 +77,13 @@ func (dc *GoDiskCache) Set(key, data string, lifetime int) error {
 	filename := "godiskcache_" + hex.EncodeToString(hasher.Sum(nil))
 
 	//open the file
-	if file, err := os.Create(os.TempDir() + filename); err == nil {
+	if file, err := os.Create(dc.directory + filename); err == nil {
 		_, err = file.Write([]byte(data))
 		_ = file.Close()
 	} //if
 
 	if err == nil {
-		dc.Keys[key] = cacheFile{fileName: filename, lifeTime: lifetime}
+		dc.keys[key] = cacheFile{fileName: filename, lifeTime: lifetime}
 	} //if
 
 	return err
