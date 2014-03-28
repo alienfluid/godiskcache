f7abc862b53555e3cfb46c9daba6ed4db35b0952
Farhan Ahmed <fahmed@cbinsights.com>
2014-03-28 16:46:37 -0400

    Benchmarking tests for godiskcache

--- go_disk_cache_test.go
+++ go_disk_cache_test.go
@@ -3,14 +3,15 @@ package godiskcache
 import (
 	"crypto/rand"
 	"fmt"
+	mrand "math/rand"
 	"sync"
 	"testing"
 )
 
-var _MAX_ITEMS int = 10000
-var _MAX_ITERATIONS int = 10
-var _MAX_KEY_LEN = 10
-var _MAX_VALUE_LEN = 10
+var _MAX_ITEMS int = 5000
+var _MAX_ITERATIONS int = 5
+var _MAX_KEY_LEN = 128
+var _MAX_VALUE_LEN = 1024
 
 func generateRandomString(nchars int) string {
 	var alphanum string = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -65,5 +66,72 @@ func TestConcurrentCacheWrite(t *testing.T) {
 		}
 
 		fmt.Println("Total: ", len(kv), " Failed: ", count)
+	}
+}
+
+func BenchmarkCacheReads(b *testing.B) {
+	var p = Params{Directory: "godiskcache/"}
+	gc := New(&p)
+
+	// Generate the test data
+	kv := buildRandomKeyValuePairs()
+
+	// Store the data in the cache concurrently
+	var w sync.WaitGroup
+	for k, v := range kv {
+		w.Add(1)
+		go addToCache(gc, k, v, &w)
+	}
+	w.Wait()
+
+	// Get a list of all keys so that we can randomly pick them
+	keys := make([]string,len(kv))
+	for k, _ := range kv {
+		keys = append(keys, k)
+	}
+
+	// Get a list of random indices since we don't want to generate them in the loop
+	inds := make([]int, len(kv))
+	for i := 0; i < len(kv); i++ {
+		inds = append(inds, mrand.Intn(len(kv)))
+	}
+
+	// Reset the timer to begin the read test
+	b.ResetTimer()
+
+	// Test loop
+	for i := 0; i < b.N; i++ {
+		_, _ = gc.Get(keys[inds[i % len(kv)]], 3600)
+	}
+}
+
+func BenchmarkCacheWrites(b *testing.B) {
+	var p = Params{Directory: "godiskcache/"}
+	gc := New(&p)
+
+	// Generate the test data
+	kv := buildRandomKeyValuePairs()
+
+	// Store the key/value pairs in lists for each access
+	keys := make([]string, len(kv))
+	values := make([]string, len(kv))
+
+	for k, v := range kv {
+		keys = append(keys, k)
+		values = append(values, v)
+	}
+
+	// Get a list of random indices since we don't want to generate them in the loop
+	inds := make([]int, len(kv))
+	for i := 0; i < len(kv); i++ {
+		inds = append(inds, mrand.Intn(len(kv)))
+	}
+
+	// Reset the timer to begin the read test
+	b.ResetTimer()
+
+	// Test loop
+	for i := 0; i < b.N; i++ {
+		_ = gc.Set(keys[inds[i % len(kv)]], values[inds[i % len(kv)]])
 	}
 }
